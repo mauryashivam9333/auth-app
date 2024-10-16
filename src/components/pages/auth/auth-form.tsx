23c68da791102c72bb682e2df50f6a4264758ee2
Shivam Maurya <mauryashivam9333@gmail.com>
2024-10-16 12:20:34 +0530

    fix: chore

--- src/components/pages/auth/auth-form.tsx
+++ src/components/pages/auth/auth-form.tsx
@@ -25,7 +25,7 @@ const AuthForm: React.FC = () => {
   return (
     <motion.div
       initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: 100 }}
       transition={{
         duration: 0.4,
